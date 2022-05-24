e34381c6281e0543df733f9f3fe9d902ae67256f
Fidellis-Taz <fidellis.dango@gmail.com>
2022-05-24 18:59:18 +0300

    bus image api fixed

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,4 @@
 //export const API = 'http://localhost:8525/api';
+export const API_BASE = "https://zupco-book.herokuapp.com/api";
 export const API = "https://zupco-book.herokuapp.com/api";
 export const API_ROOT = 'http://localhost:8525';
